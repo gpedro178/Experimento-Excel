c3c95abb06d5f5e883c77ceb23b1d51c4cc9210c
GPedro <gpedro178@gmail.com>
2020-12-29 20:15:49 -0300

    Practice Tasks Completed

--- practica_excel.py
+++ practica_excel.py
@@ -31,12 +31,53 @@ for provincia in clientesProvincia[1:]:
         
         clientesEn_BSAS += 1
         
-        indice = clientesProvincia.index(provincia)
+        indiceProvincia = clientesProvincia.index(provincia)
         
         clientesGasto_total_super_BSAS += float(
-            clientesSupermercado[indice].value)
+            clientesSupermercado[indiceProvincia].value)
 
 clientesGasto_prom_super_BSAS = round( clientesGasto_total_super_BSAS / clientesEn_BSAS, 2)
 
 print("Gasto Promedio en Supermercado de Clientes que Viven en BS AS: $", clientesGasto_prom_super_BSAS)
 print()
+
+############
+# Quiénes gastan más en total? solteros o casados?
+############
+
+clientesEstado_civil = excelSheet["W"]
+# La columna "C" o "INGRESO" es equivalente a la suma del gasto en todas las otras columnas por lo que se usa como gasto total.
+clientesGasto_total = excelSheet["C"]
+
+clientesGasto_total_solteros = 0
+clientesGasto_total_casados = 0
+
+for estado in clientesEstado_civil[1:]:
+    if estado.value == "Soltero":
+
+        indiceEstado = clientesEstado_civil.index(estado)
+
+        clientesGasto_total_solteros += float(
+            clientesGasto_total[indiceEstado].value)
+
+    elif estado.value == "Casado":
+
+        indiceEstado = clientesEstado_civil.index(estado)
+
+        clientesGasto_total_casados += float(
+            clientesGasto_total[indiceEstado].value)
+
+print("Gasto Total de Solteros: $", round(
+    clientesGasto_total_solteros,2))
+print("Gasto Total de Casados: $", round(
+    clientesGasto_total_casados,2))
+print()
+
+# Determinando qué grupo consume más
+
+if clientesGasto_total_casados > clientesGasto_total_solteros:
+    print("Los clientes casados consumen más en total que los clientes solteros")
+elif clientesGasto_total_casados < clientesGasto_total_solteros:
+    print("Los clientes solteros consumen más en total que los clientes casados")
+else:
+    print("Los clientes casados y solteros tienen el mismo nivel de consumo total")
