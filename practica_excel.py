fb4d8fc37cea81a2f69e4526706da8cc447c9464
GPedro <gpedro178@gmail.com>
2020-12-29 13:11:51 -0300

    Some initial testing code

--- practica_excel.py
+++ practica_excel.py
@@ -1,1 +1,9 @@
-#text texty text
+import openpyxl
+excelWorkbook = openpyxl.load_workbook("Experimento-Excel/MuestraTarjetaX2019.xlsx")
+excelSheet = excelWorkbook.active
+
+print("Total Filas:", excelSheet.max_row)
+print("Total Columnas:", excelSheet.max_column)
+print()
+
+clientesProvincia = excelSheet["B"]
